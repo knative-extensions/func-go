4ce2b93888ded465b1a2180310bb641c2abd15fc
Luke Kingland <luke@lukekingland.com>
2023-04-03 14:47:47 +0900

    cloudevents signatures

--- go/cmd/fhttp/main.go
+++ go/cmd/fhttp/main.go
@@ -9,16 +9,35 @@ import (
 	fn "github.com/lkingland/func-runtimes/go/http"
 )
 
+// Main illustrates how scaffolding works to wrap a user's function.
 func main() {
-	i := fn.DefaultHandler{Handler: Handle}
-	err := fn.Start(i)
-	if err != nil {
+	// Instanced example (in scaffolding, 'New()' will be in module 'f')
+	if err := fn.Start(New()); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
+
+	// Static example (in scaffolding 'Handle' will be in module f
+	// if err := fn.Start(fn.DefaultHandler{Handle}); err != nil {
+	// 	fmt.Fprintln(os.Stderr, err.Error())
+	// 	os.Exit(1)
+	// }
 }
 
-// Example Static Handler
+// Example Static HTTP Handler implementation.
 func Handle(ctx context.Context, res http.ResponseWriter, req *http.Request) {
-	fmt.Println("Hello World")
+	fmt.Println("Static HTTP handler invoked")
+	res.Write([]byte("Static HTTP handler invoked\n"))
+}
+
+// MyFunction is an example instanced HTTP function implementation.
+type MyFunction struct{}
+
+func New() *MyFunction {
+	return &MyFunction{}
+}
+
+func (f *MyFunction) Handle(_ context.Context, res http.ResponseWriter, req *http.Request) {
+	fmt.Println("Instanced HTTP handler invoked")
+	res.Write([]byte("Instanced HTTP handler invoked\n"))
 }
