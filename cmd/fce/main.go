01be274a94bfe3f33ff9a7a289130fbdfa5d1a0a
Luke Kingland <luke@lukekingland.com>
2023-06-09 10:46:27 +0900

    update test cmd imports

--- cmd/fce/main.go
+++ cmd/fce/main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/cloudevents/sdk-go/v2/event"
 
-	ce "github.com/lkingland/func-runtimes/go/cloudevents"
+	ce "github.com/lkingland/func-runtime-go/cloudevents"
 )
 
 // Main illustrates how scaffolding works to wrap a user's function.
