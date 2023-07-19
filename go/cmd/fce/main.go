7aa52fd39c27c673110434c377ec52020304f1f9
Luke Kingland <luke@lukekingland.com>
2023-07-19 13:42:03 +0900

    feat: cloudevents signatures (#14)
    cloudevents signatures

--- go/cmd/fce/main.go
+++ go/cmd/fce/main.go
@@ -7,49 +7,55 @@ import (
 
 	"github.com/cloudevents/sdk-go/v2/event"
 
-	fn "github.com/lkingland/func-runtimes/go/cloudevents"
+	ce "github.com/lkingland/func-runtimes/go/cloudevents"
 )
 
+// Main illustrates how scaffolding works to wrap a user's function.
 func main() {
-	// Static:
-	i := fn.DefaultHandler{Handle}
-	err := fn.Start(i)
-	if err != nil {
+	// Instanced Example
+	// (in scaffolding 'New()' will be in module 'f')
+	if err := ce.Start(New()); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 
-	// Instanced
-
+	// Static Example
+	// (in scaffolding 'Handle' will be in the module 'f')
+	// if err := ce.Start(ce.DefaultHandler{Handle}); err != nil {
+	//   fmt.Fprintln(os.Stderr, err.Error())
+	//   os.Exit(1)
+	// }
 }
 
-// Handle an event
+// Handle is an example static function implementation.
 func Handle(ctx context.Context, e event.Event) (*event.Event, error) {
-	/*
-	 * YOUR CODE HERE
-	 *
-	 * Try running `go test`.  Add more test as you code in `handle_test.go`.
-	 */
-
-	fmt.Println("Received event")
-	fmt.Println(e) // echo to local output
+	fmt.Println("Static CE Handler invoked")
 	return &e, nil // echo to caller
 }
 
+// MyFunction is an example instanced CloudEvents function implementation.
+type MyFunction struct{}
+
+func New() *MyFunction {
+	return &MyFunction{}
+}
+
 /*
-Other supported function signatures:
+Supported method signatures:
 
-  Handle()
-  Handle() error
-  Handle(context.Context)
-  Handle(context.Context) error
-  Handle(event.Event)
-  Handle(event.Event) error
-  Handle(context.Context, event.Event)
-  Handle(context.Context, event.Event) error
-  Handle(event.Event) *event.Event
-  Handle(event.Event) (*event.Event, error)
-  Handle(context.Context, event.Event) *event.Event
-  Handle(context.Context, event.Event) (*event.Event, error)
-
+	Handle()
+	Handle() error
+	Handle(context.Context)
+	Handle(context.Context) error
+	Handle(event.Event)
+	Handle(event.Event) error
+	Handle(context.Context, event.Event)
+	Handle(context.Context, event.Event) error
+	Handle(event.Event) *event.Event
+	Handle(event.Event) (*event.Event, error)
+	Handle(context.Context, event.Event) *event.Event
+	Handle(context.Context, event.Event) (*event.Event, error)
 */
+func (f *MyFunction) Handle() {
+	fmt.Println("Instanced CloudEvents handler invoked")
+}
