e0ba3b816445692ac110c9c4413fcdcdfe529fdf
Matthias Wessendorf <mwessend@redhat.com>
2024-01-09 10:45:37 +0100

    :mag: Logging for CloudEvents package
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

---
+++ cloudevents/logs.go
@@ -0,0 +1,23 @@
+package cloudevents
+
+import "github.com/rs/zerolog"
+
+func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	SetLogLevel(DefaultLogLevel)
+}
+
+type logLevel zerolog.Level
+
+const (
+	LogDebug    = logLevel(zerolog.DebugLevel)
+	LogInfo     = logLevel(zerolog.InfoLevel)
+	LogWarn     = logLevel(zerolog.WarnLevel)
+	LogDisabled = logLevel(zerolog.Disabled)
+)
+
+// SetLogLevel to LogDebug, LogInfo, LogWarn, or LogDisabled
+// Errors are always returned as values.
+func SetLogLevel(l logLevel) {
+	zerolog.SetGlobalLevel(zerolog.Level(l))
+}
