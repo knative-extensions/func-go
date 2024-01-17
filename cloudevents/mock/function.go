f508cf67350522da4823f9392a3bd1c61cf7673a
Matthias Wessendorf <mwessend@redhat.com>
2024-01-17 15:27:54 +0100

    Adding a simple mock for a common CE function (#41)
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

---
+++ cloudevents/mock/function.go
@@ -0,0 +1,33 @@
+package mock
+
+import (
+	"context"
+	"github.com/cloudevents/sdk-go/v2/event"
+)
+
+type Function struct {
+	OnStart  func(context.Context, map[string]string) error
+	OnStop   func(context.Context) error
+	OnHandle func(context.Context, event.Event) (*event.Event, error)
+}
+
+func (f *Function) Start(ctx context.Context, cfg map[string]string) error {
+	if f.OnStart != nil {
+		return f.OnStart(ctx, cfg)
+	}
+	return nil
+}
+
+func (f *Function) Stop(ctx context.Context) error {
+	if f.OnStop != nil {
+		return f.OnStop(ctx)
+	}
+	return nil
+}
+
+func (f *Function) Handle(ctx context.Context, event event.Event) (*event.Event, error) {
+	if f.OnHandle != nil {
+		return f.OnHandle(ctx, event)
+	}
+	return nil, nil
+}
