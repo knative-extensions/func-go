02ef4f46939561670824b1f34e128b882851a07e
Matthias Wessendorf <mwessend@redhat.com>
2024-01-17 16:13:19 +0100

    Adding simple CE start test
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- cloudevents/service_test.go
+++ cloudevents/service_test.go
@@ -1,10 +1,72 @@
 package cloudevents
 
 import (
+	"context"
+	"knative.dev/func-go/cloudevents/mock"
 	"testing"
+	"time"
 )
 
-// TestStart
-func TestStart(t *testing.T) {
-	// TODO
+// TestStart_Invoked ensures that the Start method of a function is invoked
+// if it is implemented by the function instance.
+func TestStart_Invoked(t *testing.T) {
+	var (
+		ctx, cancel = context.WithCancel(context.Background())
+		startCh     = make(chan any)
+		errCh       = make(chan error)
+		onStart     = func(_ context.Context, _ map[string]string) error {
+			startCh <- true
+			return nil
+		}
+	)
+	defer cancel()
+
+	f := &mock.Function{OnStart: onStart}
+
+	go func() {
+		if err := New(f).Start(ctx); err != nil {
+			errCh <- err
+		}
+	}()
+
+	select {
+	case <-time.After(500 * time.Millisecond):
+		t.Fatal("function failed to notify of start")
+	case err := <-errCh:
+		t.Fatal(err)
+	case <-startCh:
+		t.Log("start signal received")
+	}
+	cancel()
+}
+
+// TestStart_Static checks that static method Start(f) is a convenience method
+// for New(f).Start()
+func TestStart_Static(t *testing.T) {
+	var (
+		startCh = make(chan any)
+		errCh   = make(chan error)
+		onStart = func(_ context.Context, _ map[string]string) error {
+			startCh <- true
+			return nil
+		}
+	)
+
+	f := &mock.Function{OnStart: onStart}
+
+	go func() {
+		if err := Start(f); err != nil {
+			errCh <- err
+		}
+	}()
+
+	select {
+	case <-time.After(500 * time.Millisecond):
+		t.Fatal("function failed to notify of start")
+	case err := <-errCh:
+		t.Fatal(err)
+	case <-startCh:
+		t.Log("start signal received")
+	}
+
 }
